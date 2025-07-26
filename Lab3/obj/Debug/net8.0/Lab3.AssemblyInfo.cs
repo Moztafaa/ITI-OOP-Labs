d2f1cf06936fa2d3770e58cbda48ab4073a0857b
Mostafa <imoztafa@gmail.com>
2025-07-26 14:24:44 +0300

    Add .gitignore

--- Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
+++ Lab3/obj/Debug/net8.0/Lab3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd14528564e046bfb4226afcce34518f0188f070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2adb70e4e342c85f25a8bfd6788099d93336380")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
